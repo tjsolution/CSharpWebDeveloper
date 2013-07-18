fed320008be24cf196aaee9980c56f44164e003b
tjsolution <trihyrax@gmail.com>
2013-07-18 12:40:59 +1000

    Refactored the animal maker class to allow for easy extension of animals.

--- src/Zoo.AnimalMaker.Core/Maker.cs
+++ src/Zoo.AnimalMaker.Core/Maker.cs
@@ -1,27 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Zoo.AnimalMaker.Core.Animals;
 
 namespace Zoo.AnimalMaker.Core
 {
-    public class Maker : IMaker
+    /// <summary>
+    /// Factory for creating animal objects based on type names.
+    /// </summary>
+    public class Maker
     {
-        /// <summary>
-        /// Decides which animal to make.
-        /// </summary>
-        public static IAnimal Make(string name)
+        private static readonly Dictionary<string, Type> AnimalList = 
+            new Dictionary<string, Type>();
+
+        static Maker()
         {
-            switch (name)
+            AutoRegisterAnimals();
+        }
+
+        /// <summary>
+        /// Creates an animal based on the given animal type name.
+        /// </summary>
+        /// <param name="typeName">Name of the type of animal to create.</param>
+        /// <returns></returns>
+        /// <exception cref="AnimalMakerException">Please specify the type of animal to create.</exception>
+        public static IAnimal Make(string typeName)
+        {
+            if (string.IsNullOrEmpty(typeName))
             {
-                case "Dog":
-                    return new Dog();
-                case "Cat":
-                    return new Cat();
-                default:
-                    return null;
+                throw new AnimalMakerException("Please specify the type of animal to create.");
             }
+
+            Type animalType = null;
+            if (AnimalList.TryGetValue(typeName, out animalType))
+            {
+                return (IAnimal)Activator.CreateInstance(animalType);
+            }
+
+            throw AnimalMakerException.Create("No type registered for {0}.", typeName);
+        }
+
+        /// <summary>
+        /// Register a new animal type with the factory.
+        /// </summary>
+        /// <typeparam name="TAnimal">The type of the animal to register.</typeparam>
+        /// <exception cref="AnimalMakerException">Throws an exception if invalid animal type is specified.</exception>
+        public static void Register<TAnimal>() 
+            where TAnimal : IAnimal, new()
+        {
+            Register(typeof(TAnimal));
+        }
+
+        private static void Register(Type type)
+        {
+            if (type.IsInterface || type.IsAbstract)
+            {
+                throw AnimalMakerException.Create("{0} is not a concrete type.", type.Name);
+            }
+
+            AnimalList.Add(type.Name, type);
+        }
+
+        /// <summary>
+        /// Automatically registers the animals in the library.
+        /// Reflection is slow so Animals can be registered manually in the constructor if necessary.
+        /// </summary>
+        private static void AutoRegisterAnimals()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var animalTypes = assembly.GetTypes()
+                .Where(IsAnimal)
+                .ToArray();
+            Array.ForEach(animalTypes, Register);
+        }
+
+        /// <summary>
+        /// Determines whether the specified type is an animal.
+        /// </summary>
+        /// <param name="type">The type to test.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified type is an animal; otherwise, <c>false</c>.
+        /// </returns>
+        private static bool IsAnimal(Type type)
+        {
+            return typeof(IAnimal).IsAssignableFrom(type) 
+                && 
+                !type.IsInterface;
         }
     }
 }
