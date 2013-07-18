01d24d9038bfda6686df1348c3b4ed82a5e84388
tjsolution <trihyrax@gmail.com>
2013-07-18 12:39:10 +1000

    Cleaned up and commented the Animal interface.

--- src/Zoo.AnimalMaker.Core/IAnimal.cs
+++ src/Zoo.AnimalMaker.Core/IAnimal.cs
@@ -5,10 +5,33 @@ using System.Text;
 
 namespace Zoo.AnimalMaker.Core
 {
+    /// <summary>
+    /// Interface for an Animal created by the Animal Maker.
+    /// </summary>
     public interface IAnimal
     {
-        string TypeName { get; set; }
-        int NumberOfLegs { get; set; }
-        decimal DailyFeedCost { get; set; }
+        /// <summary>
+        /// Gets the name of the Animal Type.
+        /// </summary>
+        /// <value>
+        /// The name of the Animal Type.
+        /// </value>
+        string TypeName { get; }
+
+        /// <summary>
+        /// Gets the number of legs for the Animal.
+        /// </summary>
+        /// <value>
+        /// The number of legs for the Animal.
+        /// </value>
+        int NumberOfLegs { get; }
+
+        /// <summary>
+        /// Gets the daily feed cost for the Animal.
+        /// </summary>
+        /// <value>
+        /// The daily feed cost for the Animal.
+        /// </value>
+        decimal DailyFeedCost { get; }
     }
 }
