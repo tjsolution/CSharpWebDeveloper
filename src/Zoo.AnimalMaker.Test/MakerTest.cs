1cf753c77f436b2cbde3e97d31913f1bcd47ff2f
tjsolution <trihyrax@gmail.com>
2013-07-18 12:46:57 +1000

    Checking in tests that were written during implementation of the project.

--- src/Zoo.AnimalMaker.Test/MakerTest.cs
+++ src/Zoo.AnimalMaker.Test/MakerTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Zoo.AnimalMaker.Core;
 using Zoo.AnimalMaker.Core.Animals;
+using Zoo.AnimalMaker.Test.TestAnimals;
 
 namespace Zoo.AnimalMaker.Test
 {
@@ -12,6 +13,10 @@ namespace Zoo.AnimalMaker.Test
         {
             var dog = Maker.Make("Dog");
             Assert.IsNotNull(dog);
+            Assert.IsInstanceOf(typeof(Dog), dog);
+            Assert.AreEqual(typeof(Dog).Name, dog.TypeName);
+            Assert.AreEqual(4, dog.NumberOfLegs);
+            Assert.AreEqual(15, dog.DailyFeedCost);
         }
 
         [TestCase()]
@@ -19,7 +24,35 @@ namespace Zoo.AnimalMaker.Test
         {
             var cat = Maker.Make("Cat");
             Assert.IsNotNull(cat);
-            Assert.IsInstanceOf(typeof(Cat),cat);
+            Assert.IsInstanceOf(typeof(Cat), cat);
+            Assert.AreEqual(typeof(Cat).Name, cat.TypeName);
+            Assert.AreEqual(4, cat.NumberOfLegs);
+            Assert.AreEqual(10, cat.DailyFeedCost);
+        }
+
+        [TestCase()]
+        [ExpectedException(typeof(AnimalMakerException))]
+        public void Maker_MakeACat_NullValue()
+        {
+            var cat = Maker.Make(null);
+        }
+
+        [TestCase()]
+        [ExpectedException(typeof(AnimalMakerException))]
+        public void Maker_MakeNoneExistantAnimal()
+        {
+            var cat = Maker.Make("Wallaby");
+        }
+
+        [TestCase()]
+        public void Maker_TypeExtension_Koala()
+        {
+            Maker.Register<Koala>();
+            var koala = Maker.Make(typeof(Koala).Name);
+            Assert.IsNotNull(koala);
+            Assert.IsInstanceOf(typeof(Koala), koala);
+            Assert.AreEqual(4, koala.NumberOfLegs);
+            Assert.AreEqual(5, koala.DailyFeedCost);
         }
     }
 }
---
+++ src/Zoo.AnimalMaker.Test/TestAnimals/Koala.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo.AnimalMaker.Core;
+
+namespace Zoo.AnimalMaker.Test.TestAnimals
+{
+    /// <summary>
+    /// Animal type used for testing Animal Maker extension functionality.
+    /// </summary>
+    class Koala : IAnimal
+    {
+        public string TypeName
+        {
+            get { return GetType().Name; }
+        }
+
+        public int NumberOfLegs
+        {
+            get { return 4; }
+        }
+
+        public decimal DailyFeedCost
+        {
+            get { return 5; }
+        }
+    }
+}
